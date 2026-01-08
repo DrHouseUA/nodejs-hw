9d499d60a1c7834a03377953566f53aeb18e21fb
Roman Kniazhyk <romanknyazhyk@gmail.com>
2026-01-08 10:32:51 +0200

    Correct resetPasswordSchema and clear deleting sessions in resetpassword logic

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -160,8 +160,6 @@ export const resetPassword = async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, 10);
   await User.updateOne({ _id: user._id }, { password: hashedPassword });
 
-  await Session.deleteMany({ userId: user._id });
-
   res.status(200).json({
     message: 'Password reset successfully',
   });
